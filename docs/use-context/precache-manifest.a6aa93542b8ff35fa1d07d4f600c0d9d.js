a6586822b05dbfaef463adcf612d26ff4400278d
Yeison Rodriguez <yrrodriguezb@gmail.com>
2022-05-02 22:37:29 -0500

    build - ejemplo useContext app

---
+++ docs/use-context/precache-manifest.a6aa93542b8ff35fa1d07d4f600c0d9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c438f5aabffe470b970",
+    "url": "/static/js/main.6c438f5a.chunk.js"
+  },
+  {
+    "revision": "be3b4d5bd5f61e5ca31b",
+    "url": "/static/js/1.be3b4d5b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2335b01ad9a0647cec52b4eafe05a28f",
+    "url": "/index.html"
+  }
+];

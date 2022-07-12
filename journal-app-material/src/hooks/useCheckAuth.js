d061d27628bd60e9c7af1ac739ab5afcc5a82e87
Yeison Rodriguez <yrrodriguezb@gmail.com>
2022-07-12 18:04:40 -0500

    Journal-App-Material - Redux - CRUD Firestore - File Upload - Seccion 20

--- journal-app-material/src/hooks/useCheckAuth.js
+++ journal-app-material/src/hooks/useCheckAuth.js
@@ -4,23 +4,25 @@ import { onAuthStateChanged } from 'firebase/auth';
 
 import { FirebaseAuth } from '../firebase/config';
 import { login, logout } from '../store/auth';
+import { startLoadingNotes } from '../store/journal';
 
 
 
 export const useCheckAuth = () => {
-  
-    const { status } = useSelector( state => state.auth );
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        
-        onAuthStateChanged( FirebaseAuth, async( user ) => {
-        if ( !user ) return dispatch( logout() );
+	const { status } = useSelector(state => state.auth);
+	const dispatch = useDispatch();
 
-        const { uid, email, displayName, photoURL } = user;
-        dispatch( login({ uid, email, displayName, photoURL }) );
-        })
-    }, []);
+	useEffect(() => {
 
-    return status;
+		onAuthStateChanged(FirebaseAuth, async (user) => {
+			if (!user) return dispatch(logout());
+
+			const { uid, email, displayName, photoURL } = user;
+			dispatch(login({ uid, email, displayName, photoURL }));
+			dispatch(startLoadingNotes());
+		})
+	}, []);
+
+	return status;
 }
